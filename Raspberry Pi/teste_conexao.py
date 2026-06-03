4dbfae48589c75877473b9dba6bf42e095ba10c6
Jvictor2109 <joaobrasil2109@gmail.com>
2026-06-03 13:04:05 +0200

    mudando o teste

--- Raspberry Pi/teste_conexao.py
+++ Raspberry Pi/teste_conexao.py
@@ -1,11 +1,6 @@
-from config import *
-from  serial_comm import *
+import serial
 
-serial = SerialComm(port="/dev/ttyS0", baudrate=115200, simulate=False)
-
-resposta = serial.send("PG")
-print("Teste conexao")
-if resposta:
-    print(resposta)
-else:
-    print("Não chegou")
+s = serial.Serial('/dev/ttyS0', 115200, timeout=2)
+s.write(b'hello\n')
+print(s.readline().decode())
+s.close()
