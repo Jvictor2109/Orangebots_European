c590911e9dd91f1fbbec60c3e6962d634ed8ee9f
Jvictor2109 <joaobrasil2109@gmail.com>
2026-06-03 16:25:22 +0200

    criando pasta raspberry sem espaço

--- ESP32/v1/v1.ino
+++ ESP32/v1/v1.ino
@@ -1,10 +1,17 @@
 void setup() {
-  Serial2.begin(115200);
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, 16, 17);
 }
 
 void loop() {
-  if (Serial2.available()) {
-    String msg = Serial2.readStringUntil('\n');
-    Serial2.println("ECHO: " + msg);
+  Serial2.println("PING");
+  delay(100);
+
+  if(Serial2.available()){
+    String resposta = Serial2.readStringUntil('\n');
+    Serial.println(resposta);
+  }
+  else{
+    Serial.println("Nada recebido");
   }
 }
