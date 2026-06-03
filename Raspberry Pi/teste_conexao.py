f46be3f93297cfaa5646df15c5031e934afc7aa8
Jvictor2109 <joaobrasil2109@gmail.com>
2026-06-03 12:23:07 +0200

    Mandando teste denovo

--- Raspberry Pi/teste_conexao.py
+++ Raspberry Pi/teste_conexao.py
@@ -4,6 +4,7 @@ from  serial_comm import *
 serial = SerialComm(port="/dev/ttyS0", baudrate=115200, simulate=False)
 
 resposta = serial.send("PG")
+print("Teste conexao")
 if resposta:
     print(resposta)
 else:
